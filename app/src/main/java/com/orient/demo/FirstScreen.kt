edb3beb2081f02b7c72f3607102bb5b62bf25af3
Orient <orientr.vip.qq.com>
2021-03-29 23:07:11 +0800

    增加：没有任务时，显示一些文字提示，增加topbar与content的分界线
    已知BUG：
    1.在edit界面可以点击FirstScreen里面的surface
    2.在edit界面直接按返回键会退出程序
    
    待完善：
    1.如果任务没有完成，删除时应该有个提示框
    2.编辑任务的界面应该和输入法一同升起
    3.增加时间分类，以及待办总数

--- app/src/main/java/com/orient/demo/FirstScreen.kt
+++ app/src/main/java/com/orient/demo/FirstScreen.kt
@@ -2,17 +2,18 @@ package com.orient.demo
 
 import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @ExperimentalFoundationApi
@@ -22,16 +23,22 @@ fun FirstScreen(viewModel: MyViewModel, showEdit: () -> Unit) {
     val scaffoldState = rememberScaffoldState()
     Scaffold(scaffoldState = scaffoldState,
         topBar = {
-             Box(modifier = Modifier
-                 .fillMaxWidth()
-                 .padding(top = 50.dp, bottom = 4.dp)) {
-                 Text(
-                     modifier = Modifier.padding(start = 20.dp),
-                     text = "待办",
-                     style = MaterialTheme.typography.h5,
-                     color = MaterialTheme.colors.onBackground
-                 )
-             }
+            Column{
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 50.dp, bottom = 4.dp)
+                ) {
+                    Text(
+                        modifier = Modifier.padding(start = 20.dp),
+                        text = "待办",
+                        style = MaterialTheme.typography.h5,
+                        color = MaterialTheme.colors.onBackground
+                    )
+                }
+                Divider(thickness = 0.8.dp)
+                //增加分界线
+            }
         },
         floatingActionButton = {
             ExtendedFloatingActionButton(text = {
@@ -46,12 +53,12 @@ fun FirstScreen(viewModel: MyViewModel, showEdit: () -> Unit) {
         }, floatingActionButtonPosition = FabPosition.End,
         content = {
             Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                events?.let {
+                if(events != null && events?.isNotEmpty() == true){
                     LazyColumn {
                         items(events!!, key = { event -> event.eid }) { event ->
                             Cards(event,
                                 update = {
-//                                    event.eventDone = !event.eventDone
+//修改自身可能导致了UI不刷新                                    event.eventDone = !event.eventDone
                                     val newEvent = event.copy(eventDone = !event.eventDone)
                                     viewModel.updateEvent(newEvent)
                                 },
@@ -62,6 +69,14 @@ fun FirstScreen(viewModel: MyViewModel, showEdit: () -> Unit) {
                                 }
                             )
                         }
+                    }
+
+                } else {
+                    Box(modifier = Modifier
+                        .padding(bottom = 40.dp)
+                        .fillMaxSize(),
+                        contentAlignment = Alignment.Center) {
+                        Text(text = "这里空空如也~\n\n1.点击卡片完成任务\n2.长按卡片删除任务",Modifier.alpha(0.5f))
                     }
                 }
             }
